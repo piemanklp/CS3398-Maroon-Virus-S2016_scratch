d0930f74d62cedc7e807dde8811586aa7fca7cda
Samuel Rios <scr3@txstate.edu>
2016-02-20 19:15:03 -0600

    Added class userInput

--- music.cpp
+++ music.cpp
@@ -7,6 +7,12 @@
 class Chords{ // Contains all major chords. Will play a certain chord upon being prompted
 			 // Seperating both the chord and scale classes makes it easier to edit each
 			// sections code.
+  public:  
+};
+
+class userInput { // Contains user entered information
+
+  public:
 
 };
 int main(){
