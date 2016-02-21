b3e1819966d7fdd91c564ec733598feb6914aa4d
Kyle Ross <kdr3@txstate.edu>
2016-02-20 19:18:34 -0600

    added a string function into the public field of chord class
    with '#' will be ignored, and an empty message aborts the commit.

--- music.cpp
+++ music.cpp
@@ -8,6 +8,7 @@ class Chords{ // Contains all major chords. Will play a certain chord upon being
 			 // Seperating both the chord and scale classes makes it easier to edit each
 			// sections code.
   public:  
+  	string chord_name;      //will hold the name of the chord, beneficial to use this method in chords instead of having dozens of differnt classes for each specific chord
 };
 
 class userInput { // Contains user entered information
