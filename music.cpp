21a17b78aed54641c29631564a0305912bcae159
Marcel <mte3@txstate.edu>
2016-02-20 19:08:11 -0600

    added chords class

--- music.cpp
+++ music.cpp
@@ -4,7 +4,11 @@
  *  Created on: Feb 20, 2016
  *      Author: Marcel Erskine
  */
+class Chords{ // Contains all major chords. Will play a certain chord upon being prompted
+			 // Seperating both the chord and scale classes makes it easier to edit each
+			// sections code.
 
+};
 int main(){
 
 	return 0;
